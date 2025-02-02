32f6a77bee9f1c0cc457fd8b74fbc50c2b7e82b5
JozBLT <jonjon.joz114@gmail.com>
2025-02-02 18:06:06 +0100

    testing data

---
+++ migrations/Version20250202160352.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250202153311 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("INSERT INTO categories (name, created_at, updated_at) VALUES 
+                                                          ('Freestyle', NOW(), NOW()), 
+                                                          ('Freeride', NOW(), NOW()), 
+                                                          ('Alpin', NOW(), NOW()), 
+                                                          ('Boardercross', NOW(), NOW())");
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("UPDATE tricks SET category_id = NULL WHERE category_id IN 
+                      (SELECT id FROM categories WHERE name IN ('Freestyle', 'Freeride', 'Alpin', 'Boardercross'))");
+
+        $this->addSql("DELETE FROM categories WHERE name IN ('Freestyle', 'Freeride', 'Alpin', 'Boardercross')");
+    }
+}
